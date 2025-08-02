86c1e3c5b26015f58c1d179dbc245cc052246c77
sulayman110 <piceofislam@gmail.com>
2025-08-03 04:40:24 +0600

    loops-pactic

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+// const numbers = [12,15,35,36,58]
+// for(const num of numbers){
+//     console.log(num)
+// }
+
+const fruits = ['orang','appel','bannana']
+for(const fruit of fruits){console.log(fruit ,'i want to eat')}
