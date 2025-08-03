696e73228fb5c536ab2948f140b6c39ccc87257c
sulayman110 <piceofislam@gmail.com>
2025-08-03 06:29:47 +0600

    pactic-while

---
+++ while.js
@@ -0,0 +1,12 @@
+// const number = 8;
+// if(number < 10){
+//     console.log('la ilaha illallah')
+    
+// let num = 0 ;
+// while(num <= 10){
+//     console.log('la ilaha illallah',num);
+//     num+=5;
+
+
+for(let num = 1; num <= 10 ; num++){
+    console.log('la ilaha illallah',num);}
