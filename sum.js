053b5e0fec00187eaacb70fef6ade767478cb7f6
sulayman110 <piceofislam@gmail.com>
2025-08-03 06:54:46 +0600

    pactic-sum

---
+++ sum.js
@@ -0,0 +1,10 @@
+// let num = 50;
+// while(num <= 100){console.log(num);num++;}
+let num = 1 ;
+let sum = 0 ;
+while(num <= 10){
+    console.log(num);
+    sum=sum+num;
+    console.log("sum:",sum)
+    num++;
+}
