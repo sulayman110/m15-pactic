068f79d731e52ecc51af01b821d201e700c182a7
sulayman110 <piceofislam@gmail.com>
2025-08-05 11:12:56 +0600

    break

--- break.js
+++ break.js
@@ -1,8 +1,15 @@
-for(let i = 1; i<150; i++){
-    if(i>100){
-        break;
+// for(let i = 1; i<150; i++){
+//     if(i>100){
+//         break;
         
+//     }
+//   console.log(i ,'allah')
+// }  
+// console.log('laillaha illallah')
+
+for(let i=0; i<=10; i++){
+    if(i % 2 === 0){
+        continue
     }
-  console.log(i ,'allah')
-}  
-console.log('laillaha illallah')
+    console.log(i)
+}
