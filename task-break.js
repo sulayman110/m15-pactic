3d8ad13be02de66841b1a08cdad715ceaa3e1bce
sulayman110 <piceofislam@gmail.com>
2025-08-05 12:33:26 +0600

    task-break-2

--- task-break.js
+++ task-break.js
@@ -1,8 +1,16 @@
 // Write a loop 1 to 200. Use break to exit the loop once you find 100
 
-for(let i = 0;i < 200; i++){
-    if(i>100){
-        break
+// for(let i = 0;i < 200; i++){
+//     if(i>100){
+//         break
+//     }
+//     console.log(i)
+// }
+//Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+let i = 1 ;
+while(i<200){
+   i++; if(i>100){
+        break;
     }
-    console.log(i)
-}
+    
+console.log(i)}
